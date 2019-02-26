a385a2d8e650fac4b6c3a478b20ff05bc382b2fd
Rafael Moraes <roliveira.moraes@gmail.com>
2019-02-26 14:58:35 -0300

    Configure redis client size to 1 and redis server size to 5

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: Rails.application.secrets[:redis_provider] }
+  config.redis = { size: 1, url: Rails.application.secrets[:redis_provider] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Rails.application.secrets[:redis_provider] }
+  config.redis = { size: 5, url: Rails.application.secrets[:redis_provider] }
 end
