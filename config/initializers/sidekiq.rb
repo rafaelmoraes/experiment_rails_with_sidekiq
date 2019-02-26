1e47874e2370fdf169aff49649a4f6c56a4bc87d
Rafael Moraes <roliveira.moraes@gmail.com>
2019-02-26 15:50:01 -0300

    Fix error "Redis::CommandError: ERR max number of clients reached"

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = if Rails.env.prod?
+  config.redis = if Rails.env.production?
                    { size: 1 }
                  else
                    { url: Rails.application.secrets[:redis_provider] }
@@ -9,7 +9,7 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = if Rails.env.prod?
+  config.redis = if Rails.env.production?
                    { size: 5 }
                  else
                    { url: Rails.application.secrets[:redis_provider] }
