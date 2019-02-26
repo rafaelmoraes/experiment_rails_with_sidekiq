8f82ba1fa2881c68def45eaae2f4aa931246369d
Rafael Moraes <roliveira.moraes@gmail.com>
2019-02-26 10:29:00 -0300

    Configure redis provider by secrets.yml file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://rails_with_sidekiq_redis_1:6379/0' }
+  config.redis = { url: Rails.application.secrets[:redis_provider] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://rails_with_sidekiq_redis_1:6379/0' }
+  config.redis = { url: Rails.application.secrets[:redis_provider] }
 end
